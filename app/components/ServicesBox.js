ad758c2d6a507f2f6282090267daaf75a911b07e
HeiseMo <tim.james.tanurhan@gmail.com>
2023-09-26 11:00:56 +0200

    text fix

--- app/components/ServicesBox.js
+++ app/components/ServicesBox.js
@@ -8,7 +8,7 @@ const ServiceBox = () => {
             <div className='cards'>
                 <div className="service-card mining">
                     <h3>Mining Ores</h3>
-                    <p>We specialize in extracting the richest ores in the galaxy. Join our mining fleet and maximize your profits.</p>
+                    <p>We specialize in extracting the richest ores in the galaxy. Reach out to us for rates.</p>
                 </div>
 
                 <div className="service-card ship-building">
