c604086d208c3544ee83519621042a6c4c878d9f
HeiseMo <tim.james.tanurhan@gmail.com>
2023-09-26 12:05:02 +0200

    chr

--- app/components/Recruitment.js
+++ app/components/Recruitment.js
@@ -6,7 +6,7 @@ function Recruitment() {
     return (
         <section id="recruitment" className="recruitment-section">
             <h2>Recruitment</h2>
-            <p>We're always on the lookout for dedicated pilots to join our ranks. If you have a passion for the stars and want to be part of a tight-knit community, Red Hand Syndicate is the place for you.</p>
+            <p>We are always on the lookout for dedicated pilots to join our ranks. If you have a passion for the stars and want to be part of a tight-knit community, Red Hand Syndicate is the place for you.</p>
             
             <div className="recruitment-details">
                 <ul>
